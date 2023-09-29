afd7512dc03165c7f4b2d8166298ac989e8bb1d7
Alvin <alvinawuor8@gmail.com>
2023-09-29 15:22:22 +0300

    Fix hyperlinks

--- layouts/layouts.php
+++ layouts/layouts.php
@@ -19,11 +19,11 @@ class layouts
             <div>
                 <header class="d-flex justify-content-center py-3">
                     <ul class="nav nav-pills">
-                        <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Features</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
+                        <li class="nav-item"><a href="index.php" class="nav-link active" aria-current="page">Home</a></li>
+                        <li class="nav-item"><a href="userDetails.php" class="nav-link">User Table</a></li>
+                        <!-- <li class="nav-item"><a href="#" class="nav-link">Pricing</a></li>
                         <li class="nav-item"><a href="#" class="nav-link">FAQs</a></li>
-                        <li class="nav-item"><a href="#" class="nav-link">About</a></li>
+                        <li class="nav-item"><a href="#" class="nav-link">About</a></li> -->
                     </ul>
                 </header>
             </div>
