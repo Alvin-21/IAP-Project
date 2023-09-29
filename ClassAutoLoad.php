801d754043e2f47915a358b1d89699adc03240be
Alvin <alvinawuor8@gmail.com>
2023-09-29 13:52:47 +0300

    Fix functionality for adding user data.

--- ClassAutoLoad.php
+++ ClassAutoLoad.php
@@ -1,7 +1,7 @@
 <?php
 
 function ClassAutoLoad($ClassName){
-    $directories = array("forms", "layouts");
+    $directories = array("forms", "layouts", "processes");
     foreach($directories AS $dir){
         $FileName = dirname(__FILE__) . DIRECTORY_SEPARATOR . $dir . DIRECTORY_SEPARATOR . $ClassName . ".php";
         if(is_readable($FileName)){
@@ -15,3 +15,6 @@ $conn = New dbConnection("PDO","localhost","iap_project","root","","3306");
 
 $OBJ_Layout = NEW layouts();
 $OBJ_Forms = NEW forms();
+$OBJ_Process = NEW process();
+
+$OBJ_Process->add_user_data($conn);
--- processes/process.php
+++ processes/process.php
@@ -2,7 +2,7 @@
 
 class process{
     public function add_user_data($conn) {
-        if(isset($_POST["signup"])){
+        if(isset($_POST["userDetails"])){
             $f_name = $_POST["f_name"];
             $l_name = $_POST["l_name"];
             $email_address = addslashes($_POST["email_address"]);
