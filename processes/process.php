9b4173720d26c934cc121e3933edeb52e252d57b
Alvin <alvinawuor8@gmail.com>
2023-09-29 13:44:45 +0300

    Add function for inserting user data to db

---
+++ processes/process.php
@@ -0,0 +1,16 @@
+<?php
+
+class process{
+    public function add_user_data($conn) {
+        if(isset($_POST["signup"])){
+            $f_name = $_POST["f_name"];
+            $l_name = $_POST["l_name"];
+            $email_address = addslashes($_POST["email_address"]);
+            $city = $_POST["city"];
+            $country = $_POST["country"];
+            $data = array('first_name' => $f_name, 'last_name' => $l_name, 'email_address' => $email_address, 'city' => $city, 'country' => $country);
+            $table = "users";
+            $conn->insert($table, $data);
+        }
+    }
+}
