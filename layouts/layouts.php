b5565b78d9d9135a2087805197e5000f35e91a8d
Alvin <alvinawuor8@gmail.com>
2023-09-29 09:49:40 +0300

    Add footer layout

--- layouts/layouts.php
+++ layouts/layouts.php
@@ -27,6 +27,20 @@ class layouts
                     </ul>
                 </header>
             </div>
-    <?php
+        <?php
+    }
+
+    public function footer()
+    {
+        ?>
+            <footer class="pt-3 mt-4 text-body-secondary border-top">
+                Copyright &copy; <?php print "IAP-Project" . " " . date("Y"); ?>
+            </footer>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+        </body>
+
+        </html>
+
+<?php
     }
 }
